6e019a9e0479a21aba89ee633cb4a300a4d678b3
Brian Jackson <bjack205@gmail.com>
2018-02-23 14:45:28 -0800

    HW4 code working. Ready for group check

--- HW4/cs223a/dynamics.py
+++ HW4/cs223a/dynamics.py
@@ -34,7 +34,24 @@ def mass_matrix(links, q):
         raise ValueError("Number of links (%d) and joints q (%d) must be equal." % (len(links), q.shape[0]))
 
     # TODO: Replace the following line with your code
-    raise NotImplementedError("mass_matrix not implemented")
+    # Variable declaration
+    n = len(links)
+    M = np.zeros((n, n))
+
+    # Loop over links
+    for l in range(len(links)):
+        # Compute the Jacobians
+        Jv = linear_jacobian(links, q, links[l].com, l)
+        Jw = angular_jacobian(links, q, l)
+
+        # Compute terms of mass matrix for one link
+        A = links[l].mass*Jv.T.dot(Jv)
+        B = Jw.T.dot(links[l].inertia).dot(Jw)
+
+        # Compute the summation
+        M += A + B
+
+    return M
 
 
 def gravity_vector(links, q, g=np.array([0, 0, -9.81])):
@@ -54,7 +71,25 @@ def gravity_vector(links, q, g=np.array([0, 0, -9.81])):
         raise ValueError("Number of links (%d) and joints q (%d) must be equal." % (len(links), q.shape[0]))
 
     # TODO: Replace the following line with your code
-    raise NotImplementedError("gravity_vector not implemented")
+    # Reshape the gravity vector so it stacks properly
+    g = g.reshape((3, 1))
+
+    # Compute the Jacobian and mg matrix for the first link
+    n = len(links)
+    Jv = linear_jacobian(links, q, links[0].com, 0)
+    J = Jv.T
+    mg = links[0].mass*g
+
+    # Compute the Jacobian and the mg matrix for the other links and append
+    for l in range(1, n):
+        Jv = linear_jacobian(links, q, links[l].com, l)
+        J = np.hstack((J, Jv.T))
+        mg = np.vstack((mg, links[l].mass*g))
+
+    # Compute the gravity vector
+    G = -J.dot(mg)
+    return G
+
 
 
 if __name__ == "__main__":
@@ -86,9 +121,40 @@ if __name__ == "__main__":
     q = np.array([0, 0.1])
     g = np.array([-9.81, 0, 0])
 
+    # Problem 2 robot
+    d1 = 2
+    th2 = np.radians(90)
+    d3 = 1.5
+    l1 = 0.5
+    l2 = 2
+
+    m1 = 1
+    C1 = np.array([0, 0, -l1])
+    I1 = np.eye(3)
+    m2 = 2
+    C2 = np.array([l2, 0, 0])
+    I2 = np.eye(3)*2
+    m3 = 3
+    C3 = np.array([0, 0, 0])
+    I3 = np.eye(3)*3
+
+    L1 = Link(a=0, alpha=0, d=None, theta=0, mass=m1, com=C1, inertia=I1)
+    L2 = Link(a=0, alpha=-np.pi/2, d=0, theta=None, mass=m2, com=C2, inertia=I2)
+    L3 = Link(a=l2, alpha=np.pi/2, d=None, theta=0, mass=m3, com=C3, inertia=I3)
+    links = [L1, L2, L3]
+
+    q = np.array([d1, th2, d3])
+    g = np.array([-9.81, 0, 0])
+
+    l = 3
+    Jv = linear_jacobian(links, q, pos_in_link=links[l-1].com, link_frame=l-1)
+    Jw = angular_jacobian(links, q, link_frame=l-1)
+    # print(np.round(Jv, 2))
+
+
     # Compute dynamic quantities
     M = mass_matrix(links, q)
     G = gravity_vector(links, q, g)
 
-    print(M)
-    print(G)
+    print(np.round(M, 2))
+    print(np.round(G, 2))
